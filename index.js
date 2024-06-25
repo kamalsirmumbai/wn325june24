6bbe20ed92f7ce7f711301c781fe05b31827d819
kamalsirmumbai <tester.amaan1nov22@gmail.com>
2024-06-25 11:37:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/save", (req, res) => {
+	const url = "mongodb+srv://classeskamalsir:2jWRUAA4Wa11g28H@cluster0.cibj4lp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+	const client = new MongoClient(url);
+	const db = client.db("wn25june24");
+	const coll = db.collection("student");
+	const record = {"name":req.body.name, "choice":req.body.choice};
+	coll.insertOne(record)
+	.then(result => res.send(result))
+	.catch(error => res.send(error));
+});
+
+
+app.listen(9000, () => { console.log("server ready @ 9000 "); } );
+
+
+
+
+
+
+
